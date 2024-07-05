f4c9e6c926b3600aa7e14c47f545e986c055ab6b
Adrian Rangel Martinez <142278016+AdrianRang@users.noreply.github.com>
2024-07-04 18:22:01 -0600

    can now move the x and pendulum 'interacts'

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ package src;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import src.NeuralNet.InputNeuron;
 import src.NeuralNet.Neuron;
@@ -10,9 +12,10 @@ import src.NeuralNet.OutputNeuron;
 public class Main {
     static double xPosition = 250;
     static double xSpeed = 0;
-    static double pendulumAngle = Math.PI/4; // Relative to vertical
+    static double pendulumAngle = 0; // Relative to vertical
     static double pendulumSpeed = 0;
     static double lastFrameTime = 0;
+    static double lastFrameX = 250;
     static double force = 0;
     static double acceleration = 0;
     static double angleAcceleration = 0;
@@ -20,7 +23,7 @@ public class Main {
     static final double GRAVITY = 0.001;
     static final double MASS = 1;
     static final double LENGTH = 60;
-    static final double FRICTION = 0;
+    static final double FRICTION = 0.001;
 
     public static void main(String[] args) {
         Neuron[] inputs = new Neuron[] {
@@ -34,7 +37,7 @@ public class Main {
             new OutputNeuron("x Speed")
         };
 
-        // Network network = new Network(inputs, 3, 3, 5, outputNeurons, 1, 1);
+        Network network = new Network(inputs, 3, 3, 5, outputNeurons, 1, 1);
 
         
         JFrame frame = new JFrame("Pendulum");
@@ -45,15 +48,32 @@ public class Main {
         frame.setBackground(Renderer.BACKGROUND_COLOR);
         frame.setVisible(true);
         frame.repaint();
+        frame.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    acceleration = 0;
+                }
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    acceleration = 0.01;
+                } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    acceleration = -0.01;
+                }
+            }
+        });
         lastFrameTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            System.out.println("Frame " + i);
-            System.out.println("angle: " + pendulumAngle);
+        for (int i = 0; i < 5000; i++) {
             frame.getContentPane().removeAll();
             frame.add(Renderer.renderGame(xPosition, pendulumAngle, 10, 790, LENGTH, 800, 600));
             frame.repaint();
             frame.revalidate();
-            gameStep();
+            System.out.println("Score " + gameStep());
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -72,16 +92,26 @@ public class Main {
         System.out.println("Hello, World!");
     }
 
-    static void gameStep() {
+    static double gameStep() {
         // double deltaTime = System.currentTimeMillis() - lastFrameTime;
 
-        force = -GRAVITY * Math.sin(pendulumAngle);
+        if (xPosition < 10 || xPosition > 790) {
+            xSpeed = 0;
+            xPosition = xPosition < 10 ? 10 : 790;
+        }
+        
+        force = -GRAVITY * Math.sin(pendulumAngle) + -acceleration * Math.cos(pendulumAngle)/10;
         force -= FRICTION * pendulumSpeed;
         angleAcceleration = force / MASS;
         pendulumSpeed += angleAcceleration;
         pendulumAngle += pendulumSpeed;
-
-
+        
+        xSpeed += acceleration;
+        xPosition += xSpeed;
+        
         lastFrameTime = System.currentTimeMillis();
+        lastFrameX = xPosition;
+
+        return 0; //TODO: Return score https://www.desmos.com/calculator/dc1lqebg9n
     }
 }
